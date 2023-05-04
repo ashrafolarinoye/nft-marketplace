76d82d06a1c56457332b221228f33160e2d33435
ashrafolarinoye <ashrafolarinoye@gmail.com>
2023-05-04 01:11:26 +0100

    header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return <div className="header absolute-center cut-po">
+    <span className="heading-gradient">Develops</span>
+  </div>;
 };
 
 export default Header;
